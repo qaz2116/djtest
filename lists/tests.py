0c91da09a51ea5dc4c1a4bbc2952c8d7dff05483
houzf <houzf@antmed.cn>
2017-12-18 13:45:22 +0800

    rename functional_tests/test.py to functional_tests/tests.py

--- lists/tests.py
+++ lists/tests.py
@@ -7,12 +7,13 @@ from lists.models import Item
 
 
 class HomePageTest(TestCase):
-    def test_use_home_template(self):
+    def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
     def test_can_save_a_POST_request(self):
-        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.client.post('/', data={'item_text': 'A new list item'})
+
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
